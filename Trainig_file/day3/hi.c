7fd553c09bfd9f36041dfc375b48c8ddee3e3353
Gouravbirwaz <gouravbirwaz@gmail.com>
2025-02-19 10:20:40 +0530

    update the files

--- Trainig_file/day3/hi.c
+++ Trainig_file/day3/hi.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 
+void print(int array[],int n,int i){
+   if(i==n){
+       return ;
+   }
+   printf("%d\t",array[i]);
+   print(array,n,++i);
+}
 
 
 int main(){
-    printf("Hello world !");
+    int array[10]={1,2,3,4,5,6};
+    print(array,6,0);
     return 0;
 }
